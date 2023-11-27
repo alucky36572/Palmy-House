d8245c4908dcd9f093de63d882e7c8cf8394d39f
alucky36572 <alucky36572@gmail.com>
2023-11-27 16:41:10 +0800

    改圖片路徑

--- reservation.js
+++ reservation.js
@@ -12,3 +12,32 @@ function calculateDateDifference() {
     // 显示结果
     document.getElementById("result").innerHTML = "(" + (daysDifference+1) + " 天 " + nightsDifference+" 晚)";
 }
+
+function updateTotal() {
+    // 获取每个下拉选择框的值
+    var adults = parseInt(document.getElementById("adults").value);
+    var children = parseInt(document.getElementById("children").value);
+    var infants = parseInt(document.getElementById("infants").value);
+
+    // 计算总人数
+    var total = adults + children + infants;
+
+    // 如果总人数超过7，则重置下拉框的值
+    if (total > 7) {
+        alert("总人数不能超过7！");
+        resetValues();
+    } else {
+        // 更新显示结果
+        document.getElementById("total").innerHTML = total;
+    }
+}
+
+function resetValues() {
+    // 将下拉框的值重置为0
+    document.getElementById("adults").value = "0";
+    document.getElementById("children").value = "0";
+    document.getElementById("infants").value = "0";
+
+    // 更新显示结果
+    document.getElementById("total").innerHTML = "0";
+}
