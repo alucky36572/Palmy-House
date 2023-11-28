d4f85a78602a79b02379fcdd547fa7d90f20f55b
alucky36572 <alucky36572@gmail.com>
2023-11-28 18:38:04 +0800

    網站統整

--- reservation.js
+++ reservation.js
@@ -10,7 +10,7 @@ function calculateDateDifference() {
     var daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
     var nightsDifference = Math.max(daysDifference, 1);
     // 显示结果
-    document.getElementById("result").innerHTML = "(" + (daysDifference+1) + " 天 " + nightsDifference+" 晚)";
+    document.getElementById("result").innerHTML = (daysDifference + 1) + " 天 " + nightsDifference + " 晚";
 }
 
 function updateTotal() {
@@ -24,7 +24,7 @@ function updateTotal() {
 
     // 如果总人数超过7，则重置下拉框的值
     if (total > 7) {
-        alert("总人数不能超过7！");
+        alert("總人數最多7位哦！");
         resetValues();
     } else {
         // 更新显示结果
@@ -40,4 +40,95 @@ function resetValues() {
 
     // 更新显示结果
     document.getElementById("total").innerHTML = "0";
+}
+
+// var count = 0;  // 初始计数值
+
+// function updateCount() {
+//     // 更新显示的计数值
+//     document.getElementById("count").innerHTML = count;
+// }
+
+// function increase() {
+//     // 加一
+//     count++;
+//     count = Math.min(1, count + 1);
+//     // 更新显示
+//     updateCount();
+// }
+
+// function decrease() {
+//     // 减一，但保证不小于零
+//     count = Math.max(0, count - 1);
+//     // 更新显示
+//     updateCount();
+// }
+
+// var count = 0;  // 初始计数值
+
+// function updateCount() {
+//     // 更新显示的计数值
+//     document.getElementById("count2").innerHTML = count;
+// }
+
+// function increase() {
+//     // 加一
+//     count++;
+//     count = Math.min(1, count + 1);
+//     // 更新显示
+//     updateCount();
+// }
+
+// function decrease() {
+//     // 减一，但保证不小于零
+//     count = Math.max(0, count - 1);
+//     // 更新显示
+//     updateCount();
+// }
+
+// var count = 0;  // 初始计数值
+
+// function updateCount() {
+//     // 更新显示的计数值
+//     document.getElementById("count3").innerHTML = count;
+// }
+
+// function increase() {
+//     // 加一
+//     count++;
+//     count = Math.min(1, count + 1);
+//     // 更新显示
+//     updateCount();
+// }
+
+// function decrease() {
+//     // 减一，但保证不小于零
+//     count = Math.max(0, count - 1);
+//     // 更新显示
+//     updateCount();
+// }
+
+var counters = {
+    count1: 0,
+    count2: 0,
+    count3: 0
+};
+
+function updateCount(id) {
+    // 更新显示的计数值
+    document.getElementById(id).innerHTML = counters[id];
+}
+
+function increase(id) {
+    // 加一
+    counters[id]++;
+    // 更新显示
+    updateCount(id);
+}
+
+function decrease(id) {
+    // 减一，但保证不小于零
+    counters[id] = Math.max(0, counters[id] - 1);
+    // 更新显示
+    updateCount(id);
 }
