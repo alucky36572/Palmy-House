55f7eca0fb3c2e4192129476347ba52fa16bc198
alucky36572 <alucky36572@gmail.com>
2024-04-23 13:30:00 +0800

    修改浮動導覽列

--- header.js
+++ header.js
@@ -1,13 +1,13 @@
-$(function(){
-    $('#navBoard').hover(function(){
-        $(this).stop(true).animate({
-            top:'0px',
-        },1500,'easeOutCubic')
-    },function(){
-        $(this).animate({
-            top:'-90px',
-        },2000,'easeOutBack')
-    })
+
+
+let lastScroll = 0;
+
+addEventListener("scroll", function () {
+    let nav = document.getElementById("navBoard");
+    const currentScroll = window.scrollY;
+    const scrollDirection = currentScroll > lastScroll ? nav.style.display = "none" : nav.style.display = "flex";
+    //   console.log(scrollDirection);
+    lastScroll = currentScroll;
 })
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -21,3 +21,15 @@ document.addEventListener('DOMContentLoaded', function () {
         hamburgerMenu.classList.toggle('cross');
     }
 });
+
+// $(function(){
+//     $('#navBoard').hover(function(){
+//         $(this).stop(true).animate({
+//             top:'0px',
+//         },1500,'easeOutCubic')
+//     },function(){
+//         $(this).animate({
+//             top:'-90px',
+//         },2000,'easeOutBack')
+//     })
+// })
